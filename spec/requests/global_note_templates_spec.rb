d345d2e7aa7225575e4f72299f0a4ee35166ae90
maeda-m <943541+maeda-m@users.noreply.github.com>
2025-05-21 17:25:06 +0900

    Add request specs for global note template role visibility and update behavior

--- spec/requests/global_note_templates_spec.rb
+++ spec/requests/global_note_templates_spec.rb
@@ -38,4 +38,80 @@ RSpec.describe 'Global Note Template', type: :request do
     json = JSON.parse(response.body)
     expect(target_template.name).to eq(json['note_template']['name'])
   end
+
+  it 'create global note template with roles visibility' do
+    login_request(user.login, user.login)
+
+    expected_roles = FactoryBot.create_list(:role, 2)
+    post '/global_note_templates',
+         params: { global_note_template:
+           { tracker_id: tracker.id, name: target_template_name,
+             description: 'Global Note template description', memo: 'Test memo', enabled: 1,
+             visibility: 'roles', role_ids: expected_roles.map(&:id) } }
+    expect(response).to have_http_status(302)
+
+    expect(target_template.visibility).to eq('roles')
+    expect(target_template.roles.count).to eq(2)
+    expect(target_template.roles.map(&:id)).to match_array(expected_roles.map(&:id))
+    expect(GlobalNoteVisibleRole.where(global_note_template_id: nil).count).to eq(0)
+  end
+
+  context 'update global note template' do
+    context 'when visibility changes from open to roles' do
+      it 'changes visibility to roles' do
+        login_request(user.login, user.login)
+        expected_roles = FactoryBot.create_list(:role, 2)
+        target = FactoryBot.create(:global_note_template, { tracker_id: tracker.id, visibility: 'open' })
+        patch '/global_note_templates/' + target.id.to_s,
+              params: { global_note_template:
+                { tracker_id: tracker.id, name: target_template_name,
+                  description: 'Global Note template description', memo: 'Test memo', enabled: 1,
+                  visibility: 'roles', role_ids: expected_roles.map(&:id) } }
+        expect(response).to have_http_status(302)
+
+        expect(target_template.visibility).to eq('roles')
+        expect(target_template.roles.count).to eq(2)
+        expect(target_template.roles.map(&:id)).to match_array(expected_roles.map(&:id))
+        expect(GlobalNoteVisibleRole.where(global_note_template_id: nil).count).to eq(0)
+      end
+    end
+
+    context 'when visibility changes from roles to open' do
+      it 'changes visibility to open' do
+        login_request(user.login, user.login)
+        roles = FactoryBot.create_list(:role, 2)
+        target = FactoryBot.create(:global_note_template, { tracker_id: tracker.id, visibility: 'roles', role_ids: roles.map(&:id) })
+        patch '/global_note_templates/' + target.id.to_s,
+              params: { global_note_template:
+                { tracker_id: tracker.id, name: target_template_name,
+                  description: 'Global Note template description', memo: 'Test memo', enabled: 1,
+                  visibility: 'open' } }
+        expect(response).to have_http_status(302)
+
+        target.reload
+        expect(target.visibility).to eq('open')
+        expect(target.roles.count).to eq(0)
+      end
+    end
+
+    context 'when changes role_ids' do
+      it 'updates roles' do
+        login_request(user.login, user.login)
+        expected_roles = FactoryBot.create_list(:role, 3)
+        target = FactoryBot.create(:global_note_template, { tracker_id: tracker.id, visibility: 'roles', role_ids: FactoryBot.create_list(:role, 2).map(&:id) })
+        patch '/global_note_templates/' + target.id.to_s,
+              params: { global_note_template:
+                { tracker_id: tracker.id, name: target_template_name,
+                  description: 'Global Note template description', memo: 'Test memo', enabled: 1,
+                  visibility: 'roles', role_ids: expected_roles.map(&:id) } }
+        expect(response).to have_http_status(302)
+
+        target.reload
+        expect(target.visibility).to eq('roles')
+        expect(target.roles.count).to eq(3)
+        expect(target.roles.map(&:id)).to match_array(expected_roles.map(&:id))
+        expect(GlobalNoteVisibleRole.where(global_note_template_id: nil).count).to eq(0)
+      end
+    end
+  end
 end
